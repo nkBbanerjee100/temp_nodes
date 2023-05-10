71d4de3773e0d0d019e5da85e19832d9b5bfb940
nkBbanerjee100 <115945840+nkBbanerjee100@users.noreply.github.com>
2023-05-10 18:18:09 +0530

    Update mod.js

--- Modules/mod.js
+++ Modules/mod.js
@@ -2,7 +2,7 @@
 const nk = 'nk';
 const paku = 'paku'
 // local var -> which I dont wanna share
-const riyanko = 'riyanko'
+const riyanko = 'r'
 console.log(module);
 // export default
 module.exports = {paku,nk};
